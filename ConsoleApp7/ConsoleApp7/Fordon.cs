68555970e0daa12441c24c3005dbc8052d333402
Admin <Admin@TEBELEVDISK>
2019-12-11 16:26:11 +0100

    skrivit en class för en fordon och set den på private

---
+++ ConsoleApp7/ConsoleApp7/Fordon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Fordon
+    {
+        private string tillverkare;
+        private string modell;
+        private int årsmodell;
+        private int vikt;
+
+
+        public void SetTillverkare(string namn)
+        {
+            tillverkare = namn;
+        }
+        public string GetTillverkare()
+        {
+            return tillverkare;
+        }
+        public void SetModell(string m)
+        {
+            modell = m;
+        }
+        public string GetModell()
+        {
+            return modell;
+        }
+        public void SetÅrsmodell(int å)
+        {
+            årsmodell = å;
+        }
+        public int GetÅrsmodell()
+        {
+            return årsmodell;
+        }
+        public void SetVikt(int v)
+        {
+            vikt = v;
+        }
+        public int GetVikt()
+        {
+            return vikt;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fordon f = new Fordon();
+            f.SetModell("M 5");
+            f.SetTillverkare("BMW");
+            f.SetÅrsmodell(2018);
+            f.SetVikt(10000);
+            Console.WriteLine(f.GetModell());
+            Console.WriteLine(f.GetTillverkare());
+            Console.WriteLine(f.GetÅrsmodell());
+            Console.WriteLine(f.GetVikt());
+
+
+
+        }
+    }
+}
